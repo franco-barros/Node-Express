c21abfca35368a1d0ccae1e920e53e7bee754125
franco-barros <fb6776928@gmail.com>
2024-12-30 19:20:10 -0300

    Routers commits

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const routerApi = require('./routes');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
+
+routerApi(app);
+
+
+// app.get('/users', (req, res) => {
+//   const { limit, offset } = req.query;
+//   if (limit && offset) {
+//     res.json({
+//       limit,
+//       offset
+//     });
+//   } else {
+//     res.send('No hay parametros');
+//   }
+// });
+
+// app.get('/categories/:categoryId/products/:productId', (req, res) => {
+//   const { categoryId, productId } = req.params;
+//   res.json({
+//     categoryId,
+//     productId,
+//   });
+// })
+
+app.listen(port, () => {
+  console.log('Mi port' +  port);
+});
