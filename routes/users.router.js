0051a11749890d0f27b8e590c9f9e3611e9b6111
franco-barros <fb6776928@gmail.com>
2024-12-30 21:38:02 -0300

    Use routes commit

---
+++ routes/users.router.js
@@ -0,0 +1,19 @@
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset,
+    });
+  } else {
+    res.send('No hay parametros');
+  }
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
+  });
+});
